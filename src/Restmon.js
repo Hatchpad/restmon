a83eec681901c2f070913d612b89953b7efddee7
Jason Mullins <jason.mullins@hatchpad.io>
2016-08-05 15:39:17 -0400

    fixed plugin capability

--- src/Restmon.js
+++ src/Restmon.js
@@ -18,7 +18,7 @@ module.exports = function(mongoose, secret) {
     this.generateMongooseSchema(schema);
     if (schemaOptions.plugins) {
       schemaOptions.plugins.forEach(function(plugin) {
-        this.schema_.plugin(plugin);
+        this.schema_.plugin(plugin.plugin, plugin.opts);
       }.bind(this));
     }
     if (doBeforeModelCreation) {
